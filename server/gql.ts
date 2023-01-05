f9aa61a5d0b71cdeeda2f39444f6c1049e33f3cc
lausamtuong <lausamtuong@gmail.com>
2023-01-05 09:34:16 +0700

    devblogObs

---
+++ server/gql.ts
@@ -0,0 +1,43 @@
+export function discussionGql(ghDiscussionCategoryId: string | undefined) {
+    return `{
+          repository(owner: "lausamtuong", name: "DevBlog") {
+              discussions(first: 100, categoryId: "${ghDiscussionCategoryId}") {
+                nodes {
+                  title
+                  url
+                  number
+                  bodyHTML
+                  bodyText
+                  createdAt
+                  lastEditedAt
+                  author {
+                    login
+                    url
+                    avatarUrl
+                  }
+                   labels(first: 100) {
+                    nodes {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+      }`
+  }
+  export function discussionDetailGql(postId: number | undefined) {
+    return `{
+      repository(owner: "lausamtuong", name: "DevBlog") {
+        discussion(number: ${postId}) {
+          title
+          bodyHTML
+          createdAt
+          author {
+            login
+            url
+            avatarUrl
+          }
+        }
+      }
+    }`
+  }
